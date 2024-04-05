52d5f90971c007d4c1b8665075561369c5a45a69
Brian DeGregorio <degregor@blackholeweb.com>
2024-04-04 20:38:38 -0600

    Converted to the fixed template actors system from Factions. Also pushing up GitHub release packages.

--- Tools/install.cmd
+++ Tools/install.cmd
@@ -3,13 +3,13 @@
 @REM Notepad++/VSCODE needs current working directory to be where Caprica.exe is 
 cd "C:\Repositories\Public\Starfield Mods\starfield-encounters-overhaul\Tools"
 
-del /s /q "D:\MO2Staging\Starfield\mods\VenworksEncountersOverhaul-Experimental\SFSE\Plugins\RealTimeFormPatcher\*.*"
-rmdir /s /q "D:\MO2Staging\Starfield\mods\VenworksEncountersOverhaul-Experimental\SFSE\Plugins\RealTimeFormPatcher"
-mkdir "D:\MO2Staging\Starfield\mods\VenworksEncountersOverhaul-Experimental\SFSE\Plugins\RealTimeFormPatcher"
+del /s /q "D:\MO2Staging\Starfield_Release\mods\VenworksEncountersOverhaul-Experimental\SFSE\Plugins\RealTimeFormPatcher\*.*"
+rmdir /s /q "D:\MO2Staging\Starfield_Release\mods\VenworksEncountersOverhaul-Experimental\SFSE\Plugins\RealTimeFormPatcher"
+mkdir "D:\MO2Staging\Starfield_Release\mods\VenworksEncountersOverhaul-Experimental\SFSE\Plugins\RealTimeFormPatcher"
 
 @echo "Deploying Main Archive to MO2 Mod DIR"
-copy /y "C:\Repositories\Public\Starfield Mods\starfield-encounters-overhaul\Dist\VenworksEncountersOverhaul - Main.ba2" "D:\MO2Staging\Starfield\mods\VenworksEncountersOverhaul-Experimental"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-encounters-overhaul\Dist\VenworksEncountersOverhaul - Main.ba2" "D:\MO2Staging\Starfield_Release\mods\VenworksEncountersOverhaul-Experimental"
 
 @echo "Deploying RTFP to MO2 Mod DIR"
-copy /y "C:\Repositories\Public\Starfield Mods\starfield-encounters-overhaul\Dist\SFSE\Plugins\RealTimeFormPatcher\VenworksCoreConfig.txt" "D:\MO2Staging\Starfield\mods\VenworksEncountersOverhaul-Experimental\SFSE\Plugins\RealTimeFormPatcher"
-copy /y "C:\Repositories\Public\Starfield Mods\starfield-encounters-overhaul\Dist\SFSE\Plugins\RealTimeFormPatcher\VenworksEncountersOverhaulConfig.txt" "D:\MO2Staging\Starfield\mods\VenworksEncountersOverhaul-Experimental\SFSE\Plugins\RealTimeFormPatcher"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-encounters-overhaul\Dist\SFSE\Plugins\RealTimeFormPatcher\VenworksCoreConfig.txt" "D:\MO2Staging\Starfield_Release\mods\VenworksEncountersOverhaul-Experimental\SFSE\Plugins\RealTimeFormPatcher"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-encounters-overhaul\Dist\SFSE\Plugins\RealTimeFormPatcher\VenworksEncountersOverhaulConfig.txt" "D:\MO2Staging\Starfield_Release\mods\VenworksEncountersOverhaul-Experimental\SFSE\Plugins\RealTimeFormPatcher"
