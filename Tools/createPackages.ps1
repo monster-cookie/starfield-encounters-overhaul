da0f8ca9eccad0d44ccbd8c29695f2602453d0dc
Brian DeGregorio <degregor@blackholeweb.com>
2026-05-11 21:35:08 -0600

    Added support for PS archives but turns out it archiver2 hasn't been updated for them or they are using PC/Xbox settings

--- Tools/createPackages.ps1
+++ Tools/createPackages.ps1
@@ -24,6 +24,10 @@ If (![System.IO.Directory]::Exists(".\Dist")) {
 & "$ENV:TOOL_PATH_ARCHIVER\Archive2.exe" ".\Staging\" -root="$PWD\Staging\" -create="$ENV:MODULE_DATABASE_PATH\$Global:ScriptingNamespaceModuleCompany-$Global:ScriptingNamespaceModuleName - Main_XBox.ba2" -format="General" -compression="XBox" -maxSizeMB=2048 -excludeFilters=".*\\meta\.ini|.*\\.*\.dds|.*\\.*\.btc|.*\\.*\.esp"
 & "$ENV:TOOL_PATH_ARCHIVER\Archive2.exe" ".\Staging\" -root="$PWD\Staging\" -create="$ENV:MODULE_DATABASE_PATH\$Global:ScriptingNamespaceModuleCompany-$Global:ScriptingNamespaceModuleName - Textures_XBox.ba2" -format="XBoxDDS" -compression="XBox" -maxSizeMB=2048 -includeFilters=".*\\.*\.dds"
 
+# Creating the PS Archives and placing then in the Dist folder (Currently Archiver2 has not been updated for PS support)
+# & "$ENV:TOOL_PATH_ARCHIVER\Archive2.exe" ".\Staging\" -root="$PWD\Staging\" -create="$ENV:MODULE_DATABASE_PATH\$Global:ScriptingNamespaceModuleCompany-$Global:ScriptingNamespaceModuleName - Main_PS.ba2" -format="General" -compression="PS" -maxSizeMB=2048 -excludeFilters=".*\\meta\.ini|.*\\.*\.dds|.*\\.*\.btc|.*\\.*\.esp"
+# & "$ENV:TOOL_PATH_ARCHIVER\Archive2.exe" ".\Staging\" -root="$PWD\Staging\" -create="$ENV:MODULE_DATABASE_PATH\$Global:ScriptingNamespaceModuleCompany-$Global:ScriptingNamespaceModuleName - Textures_PS.ba2" -format="XBoxDDS" -compression="PS" -maxSizeMB=2048 -includeFilters=".*\\.*\.dds"
+
 Write-Host -ForegroundColor Cyan "`n`n"
 Write-Host -ForegroundColor Cyan "**************************************************"
 Write-Host -ForegroundColor Cyan "**     BA2 Windows and XBox Archives Created    **"
